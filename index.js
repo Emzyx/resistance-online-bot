4e35d07058e74ab43b42618ae07697f051e5bf6c
Carlos Yepes <carlos.d.yepes.22@dartmouth.edu>
2020-06-09 17:04:19 -0400

    added delete existing role/channel when new sessionID is free

--- index.js
+++ index.js
@@ -14,11 +14,19 @@ client.on('message', message => {
     }
     const server = message.guild;
 
-    // listen for webhook message from Mad Chad
+    const role = server.roles.cache.find(role => role.name === message.content)
+    // listen for webhook message from Mad Chad to create role/channel
     if (message.webhookID) {
+        // role already exists
+        if (role) {
+            role.delete();
+            const voice = server.channels.cache.find(channel => channel.name === message.content && channel.type === 'voice')
+            voice.delete();
+        }
+        // if role/channel does not already exist
         var finalA = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
         server.roles.create({ data: {name: message.content, color: finalA, permissions: []}})
-            .then((role) => {
+            .then((new_role) => {
                 server.channels.create(message.content, { type: 'voice' })
                 .then((chan) => {
                     const category = server.channels.cache.find(c => c.name === 'Voice' && c.type === 'category');
@@ -30,7 +38,7 @@ client.on('message', message => {
                                     deny: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'],
                                 },
                                 {
-                                    id: role.id,
+                                    id: new_role.id,
                                     allow: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'],
                                 },
                             ]);
@@ -46,10 +54,10 @@ client.on('message', message => {
             .catch((error) => {
                 console.log(error);
             })
-        message.delete().catch((error) => console.log(error));   
+        message.delete().catch((error) => console.log(error));        
     }
     else { // if a user message, then the webhook has probably been sent, and the VC and role created
-        const role = server.roles.cache.find(role => role.name === message.content)
+        // const role = server.roles.cache.find(role => role.name === message.content)
         if (role) {
             message.member.roles.add(role);
             message.delete().catch((error) => console.log(error));
